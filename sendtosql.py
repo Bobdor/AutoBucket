63cc68b6eeb73e4237f91407a1d3c87ce92c664e
Bobdor <39954258+Bobdor@users.noreply.github.com>
2019-06-06 22:50:26 -0400

    Update sendtosql.py

--- sendtosql.py
+++ sendtosql.py
@@ -1,18 +1,29 @@
-import sqlite3
 import serial
+import sqlite3
+from datetime import datetime
 
-# open database
-conn=sqlite3.connect('autobucket.sqlite')
+conn = sqlite3.connect('autobucket.sqlite')
 cursor = conn.cursor()
+print('Opened Database')
+try:
+    cursor.execute('''CREATE TABLE DATA
+        (ID           INTEGER PRIMARY KEY NOT NULL,
+        TIME          DATETIME DEFAULT CURRENT_TIMESTAMP,
+        PH            NUMERIC    NOT NULL,
+        TAIR          NUMERIC    NOT NULL);''')
+    cursor.close()
+    print('Created Table')
+except:
+    print('Table already exists')
 
-# open serial port
+def getData():
+        with serial.Serial('/dev/ttyUSB0', 9600, timeout=10) as ser:
+                while 1==1:
+                        data=ser.readline().decode('utf8')
+                        ph = data.split(':')[1]
+                        airTemp = data.split(':')[0]
+                        params = (ph,airTemp)
+                        cursor.execute("INSERT INTO DATA (PH,TAIR) VALUES (?,?)", params);
+                        conn.commit()
 
-with serial.Serial('/dev/ttyUSB0', 9600, timeout=10) as ser:
-    data=ser.readline()
-    
-
-# make a loop to waiting for the data to be sent from the serial connection
-
-
-conn.commit()
-conn.close()
+getData()
