201e6cc875d468212d1a7a450be41f1c44db4b91
Bobdor <39954258+Bobdor@users.noreply.github.com>
2019-07-28 14:34:30 -0400

    Update arduino.ino

--- arduino.ino
+++ arduino.ino
@@ -1,16 +1,27 @@
 #include <Adafruit_Si7021.h>
 #include <Wire.h>
+#include <Stepper.h>
 
 // distance probe pins
-#define trigPin0 41
-#define echoPin0 5
-#define trigPin1 6
-#define echoPin1 7
+#define trigPin0 2
+#define echoPin0 3
+
+/* water level, if used
+#define trigPin1 4
+#define echoPin1 5
+*/
+
+//stepper motor
+#define STEPS 510 // the number of steps in one revolution of your motor (28BYJ-48)
+Stepper stepper(STEPS, 6, 12, 7, 13);
 
 // relays
-#define RELAY1 2
+#define RELAY1 4
+#define RELAY2 5
+#define RELAY3 8
+#define RELAY4 15
 
-//define pins
+//define pins for pumps
 int motorPin1 = 9;
 int motorPin2 = 10;
 int motorPin3 = 11;
@@ -22,9 +33,9 @@ int incomingByte=0;
 
 //set vars for ph sensor
 
-const int phPin = A0; 
-int phSensorValue = 0; 
-unsigned long int avgValue; 
+const int phPin = A0;
+int phSensorValue = 0;
+unsigned long int avgValue;
 float b;
 int buf[10],temp;
 
@@ -36,17 +47,21 @@ void setup()
   Wire.begin();
   pinMode(trigPin0, OUTPUT);
   pinMode(echoPin0, INPUT);
-  pinMode(trigPin1, OUTPUT);
-  pinMode(echoPin1, INPUT);
+//  uncomment for water level  
+//  pinMode(trigPin1, OUTPUT);
+//  pinMode(echoPin1, INPUT);
   pinMode(motorPin1, OUTPUT);
   pinMode(motorPin2, OUTPUT);
   pinMode(motorPin3, OUTPUT);
   pinMode(RELAY1,OUTPUT);
+  pinMode(RELAY2,OUTPUT);
+  pinMode(RELAY3,OUTPUT);
+  pinMode(RELAY4,OUTPUT);  
 }
 
 void loop()
 {
-  /* 
+  /*
    This first block checks to see if there is an imput from serial and does something
    0 - pump 1ml liquid from pump on pin 9
    1 - pump 1ml liquid from pump on pin 10
@@ -67,9 +82,9 @@ void loop()
    N - Relay 8 off
    O - Raise light
    P - Lower light
-  
+
   */
-  
+
   if (Serial.available() > 0 ){
      incomingByte = Serial.read();
      if (incomingByte == 48){
@@ -98,9 +113,43 @@ void loop()
       Serial.println(incomingByte);
       digitalWrite(RELAY1,LOW);
      }
-     
+      if (incomingByte == 67){
+      Serial.println(incomingByte);
+      digitalWrite(RELAY2,HIGH);
+     }
+      if (incomingByte == 68){
+      Serial.println(incomingByte);
+      digitalWrite(RELAY2,LOW);
+     }
+      if (incomingByte == 69){
+      Serial.println(incomingByte);
+      digitalWrite(RELAY3,HIGH);
+     }
+      if (incomingByte == 70){
+      Serial.println(incomingByte);
+      digitalWrite(RELAY3,LOW);
+     }    
+      if (incomingByte == 71){
+      Serial.println(incomingByte);
+      digitalWrite(RELAY4,HIGH);
+     }
+      if (incomingByte == 72){
+      Serial.println(incomingByte);
+      digitalWrite(RELAY4,LOW);
+     }           
+      if (incomingByte == 79){
+      Serial.println(incomingByte);
+      stepper.setSpeed(10); // 10 rpm
+      stepper.step(STEPS); // do 400 steps -- corresponds to one revolution in one minute
+     }
+      if (incomingByte == 80){
+      Serial.println(incomingByte);        
+      stepper.setSpeed(10); // 10 rpm
+      stepper.step(-STEPS); // do 400 steps -- corresponds to one revolution in one minute
+     }
+
       else{
-        //Serial.println('Not recognized');
+       //Serial.println(incomingByte);
       }
   }
   else {
@@ -108,7 +157,7 @@ void loop()
 
 
 
-  
+
   //plant distance probe
   long duration0, plantDistance;
   digitalWrite(trigPin0, LOW);  // Added this line
@@ -119,7 +168,7 @@ void loop()
   duration0 = pulseIn(echoPin0, HIGH);
   plantDistance = (duration0/2) / 29.1;
 
-  //water distance probe
+  /*water distance probe
   long duration1, waterDistance;
   digitalWrite(trigPin1, LOW);  // Added this line
   delayMicroseconds(2); // Added this line
@@ -128,19 +177,22 @@ void loop()
   digitalWrite(trigPin1, LOW);
   duration1 = pulseIn(echoPin1, HIGH);
   waterDistance = (duration1/2) / 29.1;
+*/
 
-  
 
   float airTemp;
   float humidity;
   double T,p0,a;
+
+  /* uncomment for turbidity
   int turValue = analogRead(A1);
   float turVolt = turValue * (5.0 / 1024.0);
+  */
 
-  //ph 
+  //ph
 
-  for(int i=0;i<10;i++) 
-   { 
+  for(int i=0;i<10;i++)
+   {
     buf[i]=analogRead(phPin);
     delay(10);
    }
@@ -158,12 +210,12 @@ void loop()
    }
    avgValue=0;
    for(int i=2;i<8;i++)
-   avgValue+=buf[i]; 
+   avgValue+=buf[i];
    float pHVol=(float)avgValue*5.0/1024/6;
    float phValue = -5.70 * pHVol + 21.34;
 
 //temperature
- 
+
 
   airTemp = sensor.readTemperature();
   humidity = sensor.readHumidity();
@@ -174,12 +226,12 @@ void loop()
   Serial.print(humidity);
   Serial.print(':');
   Serial.print(phValue);
+//  Serial.print(':');
+//  Serial.print(turVolt);
   Serial.print(':');
-  Serial.print(turVolt);
-  Serial.print(':');
-  Serial.print(plantDistance);
-  Serial.print(':');
-  Serial.println(waterDistance);   
+  Serial.println(plantDistance);
+//  Serial.print(':');
+//  Serial.println(waterDistance);
 
   delay(100);  // Pause for 10 seconds.
   }
